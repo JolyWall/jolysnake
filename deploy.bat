1bb5863a4dfadd0f2b7d06c2be1d32e6c4f20ed6
JolyWall <polikutin2006@mail.ru>
2026-05-12 21:12:38 +0300

    Disable browser touch scroll on game page

--- deploy.bat
+++ deploy.bat
@@ -6,22 +6,31 @@ setlocal
 set PYTHONUTF8=1
 set HERE=%~dp0
 
-echo === [1/4] Сборка статики через pygbag ===
+echo === [1/5] Сборка статики через pygbag ===
 "%HERE%.venv\Scripts\python.exe" -m pygbag --build --disable-sound-format-error "%HERE%"
 if errorlevel 1 goto :err
 
-echo === [2/4] Копирование build\web в docs ===
+echo === [2/5] Копирование build\web в docs ===
 if exist "%HERE%docs" rmdir /S /Q "%HERE%docs"
 xcopy /E /I /Y "%HERE%build\web" "%HERE%docs" > nul
 if errorlevel 1 goto :err
 
-echo === [3/4] Коммит ===
+echo === [3/5] Патч index.html: отключаем браузерный скролл на тач ===
+powershell -NoProfile -Command ^
+  "$p='%HERE%docs\index.html';" ^
+  "$c=Get-Content $p -Raw -Encoding UTF8;" ^
+  "$inject='    <style>html,body{margin:0;padding:0;overflow:hidden;overscroll-behavior:none;touch-action:none;height:100%%;width:100%%;-webkit-user-select:none;user-select:none}canvas{touch-action:none !important}</style>'+[char]10+'</head>';" ^
+  "if ($c -notmatch 'touch-action: none') { $c = $c -replace '</head>', $inject; [System.IO.File]::WriteAllText($p,$c,[System.Text.UTF8Encoding]::new($false)) }"
+if errorlevel 1 goto :err
+
+echo === [4/5] Коммит ===
 git add docs main.py game.py render.py config.py effects.py assets pygbag.ini
 git commit -m "Update web build"
 
-echo === [4/4] Push ===
+echo === [5/5] Push ===
 git push
 
+echo.
 echo Готово. Игра на https://jolywall.github.io/jolysnake/
 goto :eof
 
